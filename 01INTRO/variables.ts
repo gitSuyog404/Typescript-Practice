acd25159bdfed37f6514f4f51d4e351eab0af0b1
gitSuyog <codesuyog0@gmail.com>
2023-03-21 12:57:39 +0545

    don't use any keyword

--- 01INTRO/variables.ts
+++ 01INTRO/variables.ts
@@ -41,3 +41,39 @@
 // console.log(isLoggedIn);
 
 // export {};
+
+// # "any" keyword
+// It is not a special type
+// It is used whenever we don't want a particular value to cause typecheckign errors.
+// Don't use "any" as it is not a good practice in typescript
+// "any" is used to get away from the syntax of typescript
+// When typescript doesn't know what value will be assigned in the future , it will put that as  "any"
+
+// When you don't specify a type, and TypeScript can't infer it from context, the compiler will typically default to any.
+// You usually want to avoid this, though, because any isn't type-checked. Use the compiler flag noImplicitAny to flag any implicit any as an error.
+
+// There is a setting in TypeScript config file that we can go for noImplicitAny that will prevent us from using any
+
+//  Example:
+// let hitMan;
+
+// function getHitMan() {
+//   return "John Wick";
+// }
+
+// hitMan = getHitMan();
+
+// export {};
+
+// ! In this case another developer can assign values of different datatype than we expected which can ruin the consistency of the program so in these cases we should use  : _datatype
+
+// let hitMan: string;
+
+// And the function shouldn't be defined like this the function should have more values for strict checking or definition.
+// function getHitMan() {
+//   return "John Wick";
+// }
+
+// hitMan = getHitMan();
+
+// export {};
