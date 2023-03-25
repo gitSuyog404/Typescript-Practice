b6aa94ceb9c2cad317c9513d95250800f209969a
gitSuyog <codesuyog0@gmail.com>
2023-03-25 15:02:09 +0545

    Enums

---
+++ 01INTRO/Enums.js
@@ -0,0 +1,16 @@
+// # Enums
+// It can help developers to restrict someone's choice with the values that are assigned
+// One easy way is to declare const
+// But this is not good code
+// const AISLE = 0;
+// const MIDDLE = 1;
+// const WINDOW = 2;
+// Using enums
+//  enum _name {}
+// The default value is always the first one and the next values are incremented
+// enum seatChoice {
+//   AISLE,
+//   MIDDLE,
+//   WINDOW,
+// }
+// const theSeat = seatChoice.AISLE;
