338ba3d90af767737255254af708373f5e32ce71
gitSuyog <codesuyog0@gmail.com>
2023-03-25 11:53:28 +0545

    Arrays in Typescript

---
+++ 01INTRO/Arrays.js
@@ -0,0 +1,18 @@
+// # Arrays
+// You cannot directly create a array like this in typescript as it will be assigned as never type which will cause error
+// const marvel = []
+// const/let  _arrayName : _datatype[] = [];
+// const marvel: string[] = [];
+// Another way of defining an array
+// const/let _arrayName: Array<_datatype> = [];
+// const DC: Array<number> = [];
+// type User = {
+//   name: string;
+//   isActive: boolean;
+// };
+// const allUsers: User[] = [];
+// allUsers.push({ name: "Wick", isActive: true });
+// One more way to define Array
+// const/let _Arrayname : _datatype [][] (Array inside array) = []
+// const MLModels: number[][] = [[255, 255, 255], []];
+// marvel.push("Ironman");
