f8f243c40db91e8a670659b6d6cbd09479d78655
gitSuyog <codesuyog0@gmail.com>
2023-03-25 22:48:02 +0545

    Abstract class in TS

---
+++ Files/abstractClass.ts
@@ -0,0 +1,26 @@
+// # Abstract Class
+
+// abstract class takePhoto {
+//   constructor(public cameraMode: string, public filter: string) {}
+
+//   abstract getSomething(): void;
+//   getReelTime(): number {
+//     return 10;
+//   }
+// }
+
+// class Instagram extends takePhoto {
+//   constructor(
+//     public cameraMode: string,
+//     public filter: string,
+//     public burst: number
+//   ) {
+//     super(cameraMode, filter);
+//   }
+
+//   getSomething(): void {
+//     console.log("Something");
+//   }
+// }
+
+// let b = new Instagram("Portrait", "test");
