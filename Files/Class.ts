b1ba9045b570c565c18a3fa3f9f5df93b419ac87
gitSuyog <codesuyog0@gmail.com>
2023-03-25 21:39:44 +0545

    Classes basics in TS

---
+++ Files/Class.ts
@@ -0,0 +1,19 @@
+// # class
+
+// class User {
+//   email: string;
+//   name: string;
+//   city: string;
+//   weapon: string = ""; // Even though you don't pass it as a parameter you will still be able to access it and it will prevent you from assigning it's value as number as we have already declared it as a string
+//   constructor(email: string, name: string, city: string) {
+//     this.email = email;
+//     this.name = name;
+//     this.city = city;
+//   }
+// }
+
+// You dont need to write email: and name: as it is wrong
+// let Cuser = new User(email:"wick@gmail.com",name:"John Wick");
+
+// let Cuser = new User("wick@gmail.com", "wick", "New York");
+// Cuser.weapon;
