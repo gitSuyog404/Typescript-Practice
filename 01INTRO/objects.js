a054594fb68aa9597c17e99df678448e5bdbbf60
gitSuyog <codesuyog0@gmail.com>
2023-03-25 10:40:58 +0545

    type aliases basics

--- 01INTRO/objects.js
+++ 01INTRO/objects.js
@@ -14,3 +14,25 @@
 // }
 // Errors that comes up in TypeScript
 // export {};
+// # Type aliases
+// "type" keyword
+// It is like creating types in typescript
+/*
+    we write type _name = {
+
+        _key : datatype;
+        _key : datatype;
+    }
+*/
+// type User = {
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+// };
+// type myString = string;  this line doesn't seem practical because there is no usecase in which you would redefine a predefined datatype
+// function createUser(user: User(this means that the user that is being passed is of User type))
+// function createUser(user: User): User {
+//   return { name: "", email: "", isActive: true };
+// }
+// createUser({ name: "", email: "", isActive: true });
+// export {};
