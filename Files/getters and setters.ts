6b6f173a2d18ba80402e0012a0bc1cc35eadac8a
gitSuyog <codesuyog0@gmail.com>
2023-03-25 22:01:44 +0545

    getters and setters in TS basics

---
+++ Files/getters and setters.ts
@@ -0,0 +1,27 @@
+// # Getters and Setters
+
+// class User {
+//   private _courseCount = 1;
+//   constructor(public email: string, public name: string, public city: string) {}
+
+//   private deleteToken() {
+//     console.log("Token deleted");
+//   }
+//   get getAppleEmail(): string {
+//     return `apple ${this.email}`;
+//   }
+
+//   get courseCount(): number {
+//     return this._courseCount;
+//   }
+
+//   set courseCount(courseNum) {
+//     if (courseNum <= 1) {
+//       throw new Error("Course count should be more than 1");
+//     }
+
+//     this._courseCount = courseNum;
+//   }
+// }
+
+// let Cuser = new User("wick@gmail.com", "wick", "New York");
