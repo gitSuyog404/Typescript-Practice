0ec5260fa2f7012a34b58ef3ca86dce7c302c54e
gitSuyog <codesuyog0@gmail.com>
2023-03-25 15:17:43 +0545

    interface

---
+++ 01INTRO/interface.js
@@ -0,0 +1,25 @@
+// # Interface
+// It is very similar to type
+// It is like a class
+// keyword "interface"
+// interface User {
+//   readonly dbID: number;
+//   email: string;
+//   userId: number;
+//   googleId?: string;
+//   startTrial: () => string;
+// Another way of making this method
+//   startTrial(): string;
+//   getCoupon(couponname: string, value: number): number;
+// }
+// const bruce: User = {
+//   dbID: 1245,
+//   email: "wayne@gmail.com",
+//   userId: 1239874350,
+//   startTrial: () => {
+//     return "BATMAN";
+//   },
+//   getCoupon: (name: "John Wick", off: 10) => {
+//     return 10;
+//   },
+// };
