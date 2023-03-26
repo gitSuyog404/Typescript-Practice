98efb977bf1382f46baa88f5b762cad8f325a403
gitSuyog <codesuyog0@gmail.com>
2023-03-26 09:23:36 +0545

    type narrowing

---
+++ Files/typeNarrowing.ts
@@ -0,0 +1,19 @@
+// # Type Narrowing
+
+// function detectType(val: number | string) {
+//  Use typeof to create a extra guard abt what kind of variable is coming up
+//   if (typeof val === "string") {
+//     return val.toLowerCase();
+//   }
+
+//   return val + 4;
+// }
+
+// function provideId(id: string | null) {
+//   if (!id) {
+//     console.log("Assign a ID");
+//     return;
+//   }
+
+//   id.toLowerCase();
+// }
